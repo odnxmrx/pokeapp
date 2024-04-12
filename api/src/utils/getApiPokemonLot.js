4f7dceee85176b93d8d264ad5e4364747cfe60e5
odnxmrx <armandopinedag@outlook.com>
2024-04-12 12:52:18 -0700

    refactor fetch data from api

--- api/src/utils/getApiPokemonLot.js
+++ api/src/utils/getApiPokemonLot.js
@@ -3,20 +3,50 @@ const mapPokemonObject = require("./mapPokemon");
 let POKEMON_LIMIT = 150;
 const API_POKEMON = `https://pokeapi.co/api/v2/pokemon`;
 
-const getApiPokemonLot = async () => {
+// const getApiPokemonLot = async () => {
+//   try {
+//     let remotePokemons = [];
+//     let i = 1;
+//     while (i <= POKEMON_LIMIT) {
+//       const response = await axios(`${API_POKEMON}/${i}`);
+
+//       const data = response.data;
+
+//       remotePokemons.push(mapPokemonObject(data));
+//       i++;
+//     }
+
+//     return remotePokemons;
+//   } catch (error) {
+//     return { error: error.message };
+//   }
+// };
+
+const getApiPokemonLot = () => {
   try {
-    let remotePokemons = [];
-    let i = 1;
-    while (i <= POKEMON_LIMIT) {
-      const response = await axios(`${API_POKEMON}/${i}`);
+    let arrayOfIterablePokemon = [];
+    fetch(`${API_POKEMON}?limit=${POKEMON_LIMIT}`)
+      .then((response) => response.json())
+      .then(function (allPokemon) {
+        // console.log('array de TODOS allPokemon: ', allPokemon); // array of all pokemon
+        allPokemon.results.forEach(function (pokemon) {
+          fetchPokemonDetail(pokemon);
+        });
+      });
 
-      const data = response.data;
+    function fetchPokemonDetail(singlePokemon) {
+      // singlePokemon = { name: 'bulbasaur', url: 'https://pokeapi.co/api/v2/pokemon/1/' }
+      let urlPokemon = singlePokemon?.url;
 
-      remotePokemons.push(mapPokemonObject(data));
-      i++;
+      fetch(urlPokemon)
+        .then((response) => response.json())
+        .then((data) => {
+          // console.log('que es data?? ', data);
+          arrayOfIterablePokemon.push(mapPokemonObject(data));
+        });
     }
 
-    return remotePokemons;
+    return arrayOfIterablePokemon;
   } catch (error) {
     return { error: error.message };
   }
