e0833680e5f3206bb4c80bab967b7186e917e903
Armando Pi <armandopinedag@outlook.com>
2024-08-02 10:55:16 -0700

    refactor external api call

--- api/src/utils/getApiPokemonLot.js
+++ api/src/utils/getApiPokemonLot.js
@@ -1,16 +1,11 @@
 const mapPokemonObject = require("./mapPokemon");
-// let POKEMON_OFFSET = 0
-// let POKEMON_LIMIT = 50;
-const API_POKEMON = `https://pokeapi.co/api/v2/pokemon`;
+const API_POKEMON_URL = `https://pokeapi.co/api/v2/pokemon`;
 
-const getApiPokemonLot = async (POKEMON_LIMIT1, POKEMON_OFFSET1) => {
-  console.log('EL PARAM limit:', POKEMON_LIMIT1);
-  console.log('param offset1: ', POKEMON_OFFSET1);
-  
+const getApiPokemonLot = async (POKEMON_LIMIT, POKEMON_OFFSET) => {  
   
   let arrayOfIterablePokemon = [];
   try {
-    const response = await fetch(`${API_POKEMON}?offset=${POKEMON_OFFSET1}&limit=${POKEMON_LIMIT1}`);
+    const response = await fetch(`${API_POKEMON_URL}?offset=${POKEMON_OFFSET}&limit=${POKEMON_LIMIT}`);
     const allPokemon = await response.json();
 
     for (const pokemon of allPokemon.results) {
@@ -20,7 +15,7 @@ const getApiPokemonLot = async (POKEMON_LIMIT1, POKEMON_OFFSET1) => {
   } catch (error) {
     return { error };
   } 
-  
+
   return arrayOfIterablePokemon;
 };
 
